eca188fac5e3615657a555f280a2fba8d68ea91e
George Crisan <georgerdp@gmail.com>
2020-04-20 21:22:06 +0100

    feat: add fonts, add story board

--- SpendingoSwift/LogoBadge/BadgeText.swift
+++ SpendingoSwift/LogoBadge/BadgeText.swift
@@ -11,7 +11,7 @@ import SwiftUI
 struct BadgeText: View {
     var body: some View {
         Text("SpendingoSwift")
-            .font(.custom("Mina-Bold", size: 40))
+            .font(.custom("Mina-Regular", size: 40))
     }
 }
 
